ceb8b114c290fa2f483a4f71c607c2ace34025cb
David Krauth <dakrauth@gmail.com>
2013-03-01 18:44:35 -0500

    Added setup.py; transitioned from google code

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+VERSION = '0.2.1' 
+
+setup(
+    name='django-swingtime',
+    version=VERSION,
+    url='http://code.google.com/p/django-swingtime/',
+    description='A Django calendaring application.',
+    long_description='Swingtime is a Django application similar to a stripped down version of iCal for Mac OS X or Google Calendar',
+    author='David A Krauth',
+    platforms=['any'],
+    packages=[
+        'swingtime',
+        'swingtime.conf'
+    ],
+)
