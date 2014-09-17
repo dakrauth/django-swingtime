9e09c4abd82644beec8f0aad51bc8f218fde5070
david <dakrauth@gmail.com>
2014-09-17 15:06:38 +0200

    Updated version info

--- swingtime/__init__.py
+++ swingtime/__init__.py
@@ -1,15 +1,5 @@
-VERSION = (0, 3, 3, 'beta', 0)
+VERSION = (0, 3, 3)
 
 #-------------------------------------------------------------------------------
 def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
-    else:
-        version = '%s %s' % (version, VERSION[3])
-        if VERSION[3] != 'final':
-            version = '%s %s' % (version, VERSION[4])
-
-    return version
+    return '.'.join([str(i) for i in VERSION])
