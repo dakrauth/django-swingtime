e32fba3f8eecfc291201b21776b9f130e152c1c3
David Krauth <dakrauth@gmail.com>
2021-12-23 11:36:02 -0500

    server docs in docker container

--- demo/urls.py
+++ demo/urls.py
@@ -1,10 +1,11 @@
 import os
 from django.contrib import admin
 from django.views.static import serve
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.views.generic import TemplateView, RedirectView
 
 doc_root = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'docs/')
+
 
 def docs(request):
     from django import http
@@ -12,14 +13,14 @@ def docs(request):
 
 
 swingtime_patterns = [
-    url(r'^$', TemplateView.as_view(template_name='intro.html'), name='demo-home'),
-    url(r'^karate/', include('karate.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^docs/$', docs, name='swingtime-docs'),
-    url(r'^docs/(?P<path>.*)$', serve, dict(document_root=doc_root, show_indexes=False))
+    re_path(r'^$', TemplateView.as_view(template_name='intro.html'), name='demo-home'),
+    re_path(r'^karate/', include('karate.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^docs/$', docs, name='swingtime-docs'),
+    re_path(r'^docs/(?P<path>.*)$', serve, dict(document_root=doc_root, show_indexes=False))
 ]
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url='/swingtime/')),
-    url(r'^swingtime/', include(swingtime_patterns)),
+    re_path(r'^$', RedirectView.as_view(url='/swingtime/')),
+    re_path(r'^swingtime/', include(swingtime_patterns)),
 ]
--- swingtime/__init__.py
+++ swingtime/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (1, 3, 0)
+VERSION = (1, 3, 1)
 
 
 def get_version():
